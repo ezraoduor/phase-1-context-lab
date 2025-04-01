5e781a98b947c63851247c4f75fe474aaef4d201
Ezra Oduor <ezrachurchill20@gmail.com>
2025-04-01 12:43:49 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeinfo){
+    return{
+        firstName: employeeinfo[0],
+        familyName: employeeinfo[1],
+        title: employeeinfo[2],
+        payPerHour: employeeinfo[3],
+        timeInEvents: [],
+        timeOutEvents:[]
+        
+    }
+
+}
+function createEmployeeRecords(employeeinfo){
+    return employeeinfo.map(createEmployeeRecord);
+
+}
+function createTimeInEvent( dateTime){
+    let[date, hour] = dateTime.split(" ")
+    this.timeInEvents.push({
+        type : 'TimeIn',
+        date,
+        hour: parseInt(hour)
+    })
+    return this;
+}
+function createTimeOutEvent( dateTime){
+    let[date, hour] = dateTime.split(" ")
+    this.timeOutEvents.push({
+        type : 'TimeOut',
+        date,
+        hour: parseInt(hour)
+    })
+    return this;
+}
+function hoursWorkedOnDate( exactdate){
+    let timeIn = this.timeInEvents.find(e => e.date === exactdate).hour
+    let timeOut = this.timeOutEvents.find(e => e.date === exactdate).hour
+    return (timeOut - timeIn) *0.01
+}
+function wagesEarnedOnDate(exactdate ){
+    return hoursWorkedOnDate.call(this , exactdate) * this.payPerHour
+
+}
+//function allWagesFor(){
+ //   let datesWorked = this.timeInEvents.map(event => event.date)
+ //   let summedwages = 0
+ //   for (let date of datesWorked){
+ //       summedwages += wagesEarnedOnDate( this ,date)
+ //   }
+ //   return summedwages 
+//}
+function calculatePayroll(employeeRecords){
+    let summedpay = 0
+    for(let employee of employeeRecords){
+        summedpay += allWagesFor.call(employee)
+    }
+    return summedpay
+}
+function findEmployeeByFirstName(collection, firstNameString){
+    for(let employee of collection){
+        if(employee.firstName === firstNameString){
+            return employee
+        }
+    }
+
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
